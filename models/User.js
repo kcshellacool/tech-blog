b56745f4689cc58e60709ea054fa4fadb26b147f
kcshellacool <hudi1007@gmail.com>
2021-11-07 20:40:52 -0800

    redo models and routers, adding pages to layouts

--- models/User.js
+++ models/User.js
@@ -1,15 +1,50 @@
-const Post = require("./Post");
-const User = require("./User");
-const Vote = require("./Vote");
-const Comment = require("./Comment");
+const {
+    Model,
+    DataTypes
+} = require("sequelize");
+const sequelize = require("../config/connection");
+const bcrypt = require("bcrypt");
 
-User.hasMany(Post, {
-  foreignKey: "user_id",
-});
+class User extends Model {
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
 
-Post.belongsTo(User, {
-  foreignKey: "user_id",
-  onDelete: "SET NULL",
-});
+User.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len: [4]
+        }
+    }
+}, {
+    hooks: {
+        async beforeCreate(newUserData) {
+            newUserData.password = await bcrypt.hash(newUserData.password, 10);
+            return newUserData;
+        },
+        async beforeUpdate(updatedUserData) {
+            updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+            return updatedUserData;
+        }
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "user"
+})
 
-module.exports = { User, Post };
+module.exports = User;
--- models/index.js
+++ models/index.js
@@ -1,15 +1,34 @@
-const Post = require("./Post");
 const User = require("./User");
-const Vote = require("./Vote");
+const Post = require("./Post");
 const Comment = require("./Comment");
 
+
 User.hasMany(Post, {
-  foreignKey: "user_id",
-});
+    foreignKey: "user_id"
+})
+
+User.hasMany(Comment, {
+    foreignKey: "user_id"
+})
 
 Post.belongsTo(User, {
-  foreignKey: "user_id",
-  onDelete: "SET NULL",
-});
+    foreignKey: "user_id"
+})
 
-module.exports = { User, Post };
+Post.hasMany(Comment, {
+    foreignKey: "post_id"
+})
+
+Comment.belongsTo(User, {
+    foreignKey: "user_id"
+})
+
+Comment.belongsTo(Post, {
+    foreignKey: "post_id"
+})
+
+module.exports = {
+    User,
+    Post,
+    Comment
+};
